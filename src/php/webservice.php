065e8f972775d19656f9ed3f28f8affe7a11336d
Zach White <auzatch@users.noreply.github.com>
2020-10-23 17:08:47 -0400

    webserver get running
    set up webserver
    return data from GET endpoint
    add eslint rule for error in windows

--- src/php/webservice.php
+++ src/php/webservice.php
@@ -13,5 +13,45 @@ http://localhost:8000/webservice.php
 
 */
 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Access-Control-Allow-Headers: Content-Type");
 
-echo 'Hello';
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+$controller = new DataController($requestMethod);
+$controller->processRequest();
+
+class DataController {
+    private $requestMethod;
+
+    public function __construct($requestMethod) {
+        $this->requestMethod = $requestMethod;
+    }
+
+    public function processRequest() {
+        switch ($this->requestMethod) {
+            case 'GET':
+                $response = $this->getData();
+                break;
+            case 'POST':
+                break;
+            default:
+                echo 'Error';
+                break;
+        }
+        header($response['status_code_header']);
+        if ($response['body']) {
+            echo $response['body'];
+        }
+    }
+
+    private function getData() {
+        $json = file_get_contents("../data/testdata.json", true);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($json);
+        return $response;
+    }
+}
+
+// echo 'Hello';
